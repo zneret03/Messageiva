6a7ddb6045473732cfdda3bc71a8e1ae9f1afb13
Ian <DrilonIan@yahoo.com>
2021-05-08 12:30:37 +0800

    adding feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+import LoginForm from "./components/LoginForm/LoginForm";
 import ChatFeed from "./components/ChatFeed/ChatFeed";
 import "./App.css";
 
 const App: React.FC = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="753471a8-c2cf-4ff3-aea0-ba1263b3dbdc"
-      userName="javascriptmastery"
-      userSecret="123123"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps: any) => (
         <ChatFeed chatFeed={chatAppProps} />
       )}
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+//import httpRequest from "../../api/httpRequest";
+import axios from "axios";
 
 interface InitialTypes {
   username: any;
@@ -12,6 +14,7 @@ const initialState: InitialTypes = {
 
 const LoginForm: React.FC = () => {
   const [{ username, password }, setState] = useState(initialState);
+  const [error, setError] = useState("");
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
@@ -19,8 +22,26 @@ const LoginForm: React.FC = () => {
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const authObject = {
+      "Project-ID": "753471a8-c2cf-4ff3-aea0-ba1263b3dbdc",
+      "User-Name": username,
+      "User-Secret": password,
+    };
+
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
+
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+      window.location.reload();
+    } catch (error) {
+      setError("Opps, incorrect credentials");
+    }
   };
 
   return (
@@ -29,19 +50,29 @@ const LoginForm: React.FC = () => {
         <h1 className="title">Chat Application</h1>
         <form onSubmit={(event) => onSubmit(event)}>
           <input
+            required
             type="text"
+            className="input"
             value={username}
             name="username"
             onChange={(event) => handleOnChange(event)}
             placeholder="Username"
           />
           <input
+            required
             type="password"
+            className="input"
             value={password}
             name="password"
             onChange={(event) => handleOnChange(event)}
             placeholder="password"
           />
+          <div style={{ textAlign: "center" }}>
+            <button className="button" type="submit">
+              Login
+            </button>
+          </div>
+          <h2 className="error">{error}</h2>
         </form>
       </div>
     </div>
